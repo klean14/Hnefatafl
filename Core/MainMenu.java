abbbb6a4822af8bea66a47a0b79f217ebc632896
Kleanthis Symeonides <2339297s@student.gla.ac.uk>
2018-07-12 12:20:48 +0100

    added Tawlbawrdd variant

--- Core/MainMenu.java
+++ Core/MainMenu.java
@@ -1,6 +1,9 @@
 package Core;
 
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,35 +12,51 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 public class MainMenu extends JFrame implements ActionListener{
 	private JButton newGame,quit;
-	private JComboBox gamesDrop;
+	private JPanel panel;
+	private JComboBox<String> gamesDrop;
 	
 	public MainMenu() {
-		getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
+		
+		panel = new JPanel();
+		panel.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        
 		this.setVisible(true);
 		this.setSize(300, 300);
 		this.setResizable(false);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 		//Show the grid in the center of the screen
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
 		
-		String[] gameStrings = {"Hnefatafl", "Tablut"};
-		gamesDrop = new JComboBox(gameStrings);
+		String[] gameStrings = {"Hnefatafl", "Tablut", "Tawlbawrdd"};
 		
+		// JComboBox
+		gamesDrop = new JComboBox<String>(gameStrings);
+		
+		// JButtons
 		newGame = new JButton("New Game");
-		newGame.addActionListener(this);
-		quit = new JButton("Quit");
-		quit.addActionListener(this);
 		newGame.setAlignmentX(CENTER_ALIGNMENT);
+		quit = new JButton("Quit");
 		quit.setAlignmentX(CENTER_ALIGNMENT);
 		
-		add(gamesDrop);
-		add(newGame);
-		add(quit);
+		// Action Listeners
+		newGame.addActionListener(this);
+		quit.addActionListener(this);
 		
+		panel.add(gamesDrop,gbc);
+		panel.add(newGame,gbc);
+		panel.add(quit,gbc);
+		
+		this.add(panel);
 		this.revalidate();
 	}
 
@@ -47,7 +66,7 @@ public class MainMenu extends JFrame implements ActionListener{
 		// If newGame button was pressed
 		if(e.getSource() == newGame) {
 			
-			Game game = new Game(gamesDrop.getSelectedItem().toString());
+			new Game(gamesDrop.getSelectedItem().toString());
 			this.setVisible(false);
 			
 		}else if (e.getSource()==quit) {
