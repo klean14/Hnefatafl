3bb6d3ea558f69f6a42d87500b5f4a2dc4a97c0f
Kleanthis Symeonides <2339297s@student.gla.ac.uk>
2018-07-18 22:27:53 +0100

    added interface for modularity

---
+++ Core/TileInterface.java
@@ -0,0 +1,26 @@
+package Core;
+
+public interface TileInterface {
+	
+	
+	/****************Getters and setters******************/
+	
+	public int getPosX();
+	public void setPosX(int posX);
+
+	public int getPosY();
+	public void setPosY(int posY);
+
+	public boolean isOccupied();
+	public void setOccupied(boolean occupied);
+
+	public Pawn getPawn();
+	public void setPawn(Pawn pawn);
+	
+	public boolean isRestricted();
+	public void setRestricted(boolean restricted);
+
+	/*****************************************************/
+
+	public String toString();
+}
