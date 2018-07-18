07fba1a0fab87a5cbdaf3c9eb6f7c3e338006953
Kleanthis Symeonides <2339297s@student.gla.ac.uk>
2018-07-18 22:26:42 +0100

    More adjustments for more modularity

--- Core/Main.java
+++ Core/Main.java
@@ -5,13 +5,13 @@ import GUI.MainMenuGUI;
 public class Main {
 	public static void main(String[] args) {
 		
-//		for (String arg : args) {
-//			
-//			if (arg.equalsIgnoreCase("-g"))  new MainMenuGUI();
-//			if (arg.equalsIgnoreCase("-c")) new Ma;
-//			
-//		}
+		for (String arg : args) {
+			
+			if (arg.equalsIgnoreCase("-g"))  new MainMenuGUI();
+			if (arg.equalsIgnoreCase("-c"))  new GameLogic("Hnefatafl",false);
+			
+		}
 		
-		MainMenuGUI menu = new MainMenuGUI();
+//		MainMenuGUI menu = new MainMenuGUI();
 	}
 }
