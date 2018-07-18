ea20f84bb66a56422d18ebf03165bd2643cdf5a4
Kleanthis Symeonides <2339297s@student.gla.ac.uk>
2018-07-18 22:28:23 +0100

    added option for command line

---
+++ GUI/MainMenuGUI.java
@@ -0,0 +1,80 @@
+package GUI;
+
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import Core.GameLogic;
+
+public class MainMenuGUI extends JFrame implements ActionListener{
+	private JButton newGame,quit;
+	private JPanel panel;
+	private JComboBox<String> gamesDrop;
+	
+	public MainMenuGUI() {
+		
+		panel = new JPanel();
+		panel.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        
+		this.setVisible(true);
+		this.setSize(300, 300);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		//Show the grid in the center of the screen
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
+		
+		String[] gameStrings = {"Hnefatafl", "Tablut", "Tawlbawrdd"};
+		
+		// JComboBox
+		gamesDrop = new JComboBox<String>(gameStrings);
+		
+		// JButtons
+		newGame = new JButton("New Game");
+		newGame.setAlignmentX(CENTER_ALIGNMENT);
+		quit = new JButton("Quit");
+		quit.setAlignmentX(CENTER_ALIGNMENT);
+		
+		// Action Listeners
+		newGame.addActionListener(this);
+		quit.addActionListener(this);
+		
+		panel.add(gamesDrop,gbc);
+		panel.add(newGame,gbc);
+		panel.add(quit,gbc);
+		
+		this.add(panel);
+		this.revalidate();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		// If newGame button was pressed
+		if(e.getSource() == newGame) {
+			
+			new GameLogic(gamesDrop.getSelectedItem().toString(),true);
+			this.setVisible(false);
+			
+		}else if (e.getSource()==quit) {
+			System.exit(0);
+		}
+		
+	}
+
+}
