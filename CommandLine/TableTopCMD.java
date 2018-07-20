4caf1ff74571c4e2c7dd58493a751aba128e997f
Kleanthis Symeonides <2339297s@student.gla.ac.uk>
2018-07-20 19:22:35 +0100

    added commandline functionality

--- CommandLine/TableTopCMD.java
+++ CommandLine/TableTopCMD.java
@@ -7,72 +7,165 @@ import Core.GameLogic;
 import Core.Pawn;
 
 public class TableTopCMD {
+	
+	private TileCLI[][] board;
+	
+	private int boardSize;
+	
+	private GameLogic game;
+	
+	/**
+	 * Method that follows the logic sequence of the game
+	 * @param pawns the arraylist of pawns
+	 * @param game GameLogic instance
+	 * @param size The size of the board
+	 */
 	public TableTopCMD(ArrayList<Pawn> pawns,GameLogic game, int size) {
-		printBoard(size,pawns);
-	}
-
-	private void printBoard(int size, ArrayList<Pawn> pawns) {
+		
+		this.game = game;
+		this.boardSize = size;
+		this.board = new TileCLI[size][size];
+		
+		createBoard();
 		
 		while(true) {
-			String board = new String();
-			boolean kakka;
-			board += "  ";
-			for(int i = 1; i <= size; i++) 
-			{
-				board += "  ";
-				board += i;
-				board += " ";
-			}
-			
-			board += System.lineSeparator();
-			board += "  ";
-			
-			for(int i = 0; i <= size*4; i++) 
-			{
-				board += "-";
-			}
-			
-			board += System.lineSeparator();
-			
-			for(int row = 0; row < size; row ++) {
-				board += Character.toString ((char) (row + 97));;
-				for(int col = 0; col <= size; col++) {
-					kakka = false;
-					board += " | ";
-					for(Pawn pawn : pawns) {
-						if(pawn.getPosX() == row && pawn.getPosY() == col) {
-							if(pawn.isKing()) {
-								board += "k";
-							}
-							else if(pawn.getPlayer().getID() == 1) {
-								board += "o";
-							}
-							else {
-								board += "a";
-							}
-							
-							kakka = true;
-						}
-						
-					}
-					
-					if(!kakka) {
-						board += " ";
-					}
-					
+			int round = game.getRound();
+			try {
+				System.out.println("Round: " + round);
+				System.out.println("Player " + ((round % 2)+1) + " plays");
+				
+				printBoard(size);
+				System.out.println("Input <yx> <yx> of the old and new position of the pawn respectively, separated by as space or type \"exit\" to exit: ");
+				// Read user input
+				Scanner reader = new Scanner(System.in);
+				String userInput = reader.nextLine();
+				
+				if(userInput.equals("exit")) {
+					new MainMenuCMD();
 				}
-				board += System.lineSeparator();
+				// Split into 2 Strings
+				String[] token = userInput.split(" ");
+				
+				// Extract the position of the pawn to be moved
+				int oldPosY =  (int)token[0].charAt(0) - 97;
+				int oldPosX = Integer.parseInt(token[0].substring(1, token[0].length())) - 1;
+				
+				game.nextRound(board,oldPosX,oldPosY);
+				
+				//Extract the position of the new position
+				int newPosY = (int)token[1].charAt(0) - 97;
+				int newPosX = Integer.parseInt(token[1].substring(1, token[1].length())) - 1;
+				
+				game.nextRound(board,newPosX,newPosY);
+			} 
+			catch (NullPointerException e) {
+				System.out.println("No pawn found");
 			}
-			board += "  ";
-			for(int i = 0; i <= size*4; i++) 
-			{
-				board += "-";
+			catch (ArrayIndexOutOfBoundsException e1) {
+				System.out.println("Wrong input. Add the old AND new position of the pawn.");
 			}
-			
-			System.out.println(board);
-			
-			Scanner reader = new Scanner(System.in);
-			String userInput = reader.nextLine();
+			catch (NumberFormatException e1) {
+				System.out.println("Wrong input. Type first a letter and second a number.");
+			}
 		}
 	}
+
+	/**
+	 * Instantiate the tiles and set the restricted ones
+	 */
+	private void createBoard() {
+		
+		for(int row = 0; row < boardSize; row++) {
+			for(int col = 0; col < boardSize; col++) {
+				board[col][row] = new TileCLI(col,row);
+				
+				//The tiles that are restricted to the pawns except for the king
+				if(row == 0 && col == 0 || row == boardSize-1 && col == 0 || row == 0 && col == boardSize-1 || row == boardSize-1 && col == boardSize-1 || row == (boardSize-1)/2 && col == (boardSize-1)/2) {
+					board[col][row].setRestricted(true);
+				}
+	
+			}
+		}
+	}
+	
+	/**
+	 * Method to print out the board in a command line
+	 * @param size is the size of the board
+	 */
+	private void printBoard(int size) {
+		
+		String boardString = new String();
+		
+		
+		boardString += "  ";
+		for(int i = 1; i <= size; i++) 
+		{
+			boardString += "  ";
+			boardString += i;
+			boardString += " ";
+		}
+		
+		boardString += System.lineSeparator();
+		boardString += "  ";
+		
+		for(int i = 0; i <= size*4; i++) 
+		{
+			boardString += "-";
+		}
+		
+		boardString += System.lineSeparator();
+		
+		for(int row = 0; row < size; row ++) {
+			// Print out the characters of the alphabet
+			boardString += Character.toString ((char) (row + 97));;
+			for(int col = 0; col <= size; col++) {
+				boardString += " | ";
+				
+				// Check if there is a pawn in this position of the board
+				Pawn tilePawn = game.findPawn(col, row);
+					if(tilePawn != null) {
+						board[col][row].setPawn(tilePawn);
+						board[col][row].setOccupied(true);
+						if(tilePawn.isKing()) {
+							boardString += "k";
+						}
+						else if(tilePawn.getPlayer().getID() == 1) {
+							boardString += "o";
+						}
+						else {
+							boardString += "a";
+						}
+					}
+					else {
+						try {
+							board[col][row].setPawn(null);
+							board[col][row].setOccupied(false);
+						} catch (ArrayIndexOutOfBoundsException e) {
+							//Do nothing
+						}
+					}
+				
+				
+				try {
+					if(!board[col][row].isOccupied()) {
+						boardString += " ";
+					}
+				} catch (ArrayIndexOutOfBoundsException e) {
+					//Do nothing
+				}
+				
+			}
+			boardString += System.lineSeparator();
+		}
+		boardString += "  ";
+		for(int i = 0; i <= size*4; i++) 
+		{
+			boardString += "-";
+		}
+		
+		System.out.println(boardString);
+		
+	}
+	
+	
 }
