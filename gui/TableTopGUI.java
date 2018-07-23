6dc49f61b7edcd30f60378084ae40c91b318a1db
Kleanthis Symeonides <2339297s@student.gla.ac.uk>
2018-07-23 18:57:55 +0100

    Added command line front end

--- gui/TableTopGUI.java
+++ gui/TableTopGUI.java
@@ -8,7 +8,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
