d8c1eec646a0c1df6e423c355f83b38a4c991303
Kleanthis Symeonides <2339297s@student.gla.ac.uk>
2018-08-03 15:45:21 +0100

    Changed gui version to start as hnefatafl by default

--- Core/Main.java
+++ Core/Main.java
@@ -1,18 +1,15 @@
 package Core;
 
 import CommandLine.MainMenuCMD;
-import GUI.MainMenuGUI;
 
 public class Main {
 	public static void main(String[] args) {
 		
 		for (String arg : args) {
 			
-			if (arg.equalsIgnoreCase("-g"))  new MainMenuGUI();
+			if (arg.equalsIgnoreCase("-g"))  new GameLogic("Hnefatafl",true);
 			if (arg.equalsIgnoreCase("-c"))  new MainMenuCMD();
 			
 		}
-		
-//		MainMenuGUI menu = new MainMenuGUI();
 	}
 }
